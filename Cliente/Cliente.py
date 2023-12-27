a2dcb2977c81b13bf9d183f3cb9d5b17e89e4a47
Dudu200313 <eduardo.silvinocabuloso23@gmail.com>
2023-12-26 22:15:32 -0300

    testando uns négocios muitos loucos eeee

--- Cliente/Cliente.py
+++ Cliente/Cliente.py
@@ -2,18 +2,25 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect(('localhost', 7777))
-print('Connectado!\n')
+try:
+    client.connect(('localhost', 7777))
+    print('Conectado!\n')
 
-namefile = str(input('Arquivo>'))
+    namefile = input('Arquivo> ')
 
-client.send(namefile.encode())
+    client.send(namefile.encode())
 
-with open(namefile, 'wb') as file:
-    while 1:
-        data = client.recv(1000000)
-        if not data:
-            break
-        file.write(data)
-        
-print(f'{namefile} recebido!\n')        
+    with open(namefile, 'wb') as file:
+        while True:
+            data = client.recv(1000000)
+            if not data:
+                break
+            file.write(data)
+
+    print(f'{namefile} recebido!\n')
+
+except Exception as e:
+    print(f"Erro durante a execução: {e}")
+
+finally:
+    client.close()
--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -1,17 +1,36 @@
 import socket
+import os
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def send_file(file_path, connection):
+    with open(file_path, 'rb') as file:
+        data = file.read(1024)
+        while data:
+            connection.sendall(data)
+            data = file.read(1024)
 
-server.bind(('localhost', 7777))
-server.listen(1)
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('localhost', 7777))
+    server.listen(1)
 
-connection, address = server.accept()
+    connection, address = server.accept()
 
-namefile = connection.recv(1024).decode()
+    namefile = connection.recv(1024).decode()
+    print(f"Recebendo arquivo: {namefile}")
 
-with open(namefile, 'rb') as file:
-    for data in file.readlines():
-        connection.send(data)
-        
-    print('Arquivo enviado!')
+    # Obtendo o caminho absoluto do arquivo
+    file_path = os.path.abspath(namefile)
+
+    # Verificando se o arquivo existe
+    if os.path.exists(file_path):
+        send_file(file_path, connection)
+        print('Arquivo enviado!')
+    else:
+        print(f"Arquivo não encontrado: {file_path}")
+
+    connection.close()
+    server.close()
+
+if __name__ == "__main__":
+    main()
 
